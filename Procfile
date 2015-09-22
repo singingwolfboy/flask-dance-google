89be9fe7142df4f56783b3193d4f409bbca6f2d9
David Baumgold <david@davidbaumgold.com>
2015-09-21 20:50:54 -0400

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn google:app --log-file=-
---
+++ google.py
@@ -0,0 +1,23 @@
+import os
+from werkzeug.contrib.fixers import ProxyFix
+from flask import Flask, redirect, url_for
+from flask_dance.contrib.google import make_google_blueprint, google
+
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersekrit")
+app.config["GOOGLE_OAUTH_CLIENT_ID"] = os.environ.get("GOOGLE_OAUTH_CLIENT_ID")
+app.config["GOOGLE_OAUTH_CLIENT_SECRET"] = os.environ.get("GOOGLE_OAUTH_CLIENT_SECRET")
+google_bp = make_google_blueprint()
+app.register_blueprint(google_bp, url_prefix="/login")
+
+@app.route("/")
+def index():
+    if not google.authorized:
+        return redirect(url_for("google.login"))
+    resp = google.get("/plus/v1/people/me")
+    assert resp.ok
+    return "You are @{email} on Google".format(email=resp.json()["emails"][0]["value"])
+
+if __name__ == "__main__":
+    app.run()
