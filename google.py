77da4a06ada1539c38213e2af32aac1ae338ee52
David Baumgold <david@davidbaumgold.com>
2015-09-21 21:32:23 -0400

    See response text as assertion failure message

--- google.py
+++ google.py
@@ -18,7 +18,7 @@ def index():
     if not google.authorized:
         return redirect(url_for("google.login"))
     resp = google.get("/plus/v1/people/me")
-    assert resp.ok
+    assert resp.ok, resp.text
     return "You are @{email} on Google".format(email=resp.json()["emails"][0]["value"])
 
 if __name__ == "__main__":
