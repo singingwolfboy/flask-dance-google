1f592f20505f60ace3e33842648a239de1138c39
Joshua <blitz04.dev@gmail.com>
2022-08-03 17:45:18 +0530

    update scopes

--- google.py
+++ google.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersekrit")
 app.config["GOOGLE_OAUTH_CLIENT_ID"] = os.environ.get("GOOGLE_OAUTH_CLIENT_ID")
 app.config["GOOGLE_OAUTH_CLIENT_SECRET"] = os.environ.get("GOOGLE_OAUTH_CLIENT_SECRET")
-google_bp = make_google_blueprint(scope=["profile", "email"])
+google_bp = make_google_blueprint(scope=["openid", "https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"])
 app.register_blueprint(google_bp, url_prefix="/login")
 
 @app.route("/")
